a3a791f2492ef10ae81b5c7388d4cfa0c81727b0
Ekaterina Bondareva <1978231@bk.ru>
2024-03-19 21:53:28 +0500

    created state with values for all project

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,23 @@ import { Filter } from "./components/Filter";
 import { Header } from "./components/Header";
 import { BookPage } from "./components/BookPage";
 
-interface State {}
+interface State {
+  allBooks: Book[];
+  booksInProgressIds: Set<string>;
+  booksDoneIds: Set<string>;
+  currentTab: Tab;
+  tags: Set<string>;
+}
 
 export function App() {
+  const state: State = {
+    allBooks: [],
+    booksInProgressIds: new Set(),
+    booksDoneIds: new Set(),
+    currentTab: "toread" as Tab,
+    tags: new Set(),
+  };
+
   return (
     <div className="app">
       <Header />
