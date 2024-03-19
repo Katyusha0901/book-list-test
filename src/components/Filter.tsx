dbca787aedae9a953345669215c54be9de575f5c
Ekaterina Bondareva <1978231@bk.ru>
2024-03-19 23:24:38 +0500

    added interface

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+interface IProps {
+  tags: Set<string>;
+  removeTag: (tag: string) => void;
+  clearTags: () => void;
+}
+
 export function Filter() {
   return <div></div>;
 }
