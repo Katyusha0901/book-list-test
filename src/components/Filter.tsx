1b7954beaa8a0bd4acfc6ed705753c5133bfc288
Ekaterina Bondareva <1978231@bk.ru>
2024-03-19 23:31:43 +0500

    added filter structure

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -9,5 +9,20 @@ interface IProps {
 export const Filter: React.FC<IProps> = ({ tags, removeTag, clearTags }) => {
   if (!tags.size) return null;
 
-  return <div></div>;
+  const tagsJSX = Array.from(tags).map((tag) => (
+    <span className="filter__tag" onClick={() => removeTag(tag)} key={tag}>
+      #{tag}
+    </span>
+  ));
+
+  return (
+    <div className="filter">
+      <span>Filtered by tags:</span>
+      {tagsJSX}(
+      <button className="filter__clear" onClick={clearTags}>
+        clear
+      </button>
+      )
+    </div>
+  );
 };
