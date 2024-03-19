f271969ded9b8a600bdca570224f7f71546f98ef
Ekaterina Bondareva <1978231@bk.ru>
2024-03-19 21:30:45 +0500

    import components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./App.css";
+import { Books } from "./components/Books";
+import { Filter } from "./components/Filter";
+import { Header } from "./components/Header";
+import { Book } from "./components/Book";
 
 function App() {
   return <div className="App"></div>;
