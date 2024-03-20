0d8c50947f5db37711124aa0566f602ea9653ce5
Ekaterina Bondareva <1978231@bk.ru>
2024-03-20 12:54:58 +0500

    added functionality for filtering books

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -15,32 +15,44 @@ interface State {
 }
 
 export class Books extends React.Component<Props, State> {
-    booksRef= React.createRef<HTMLDivElement>()
-    filteredBooksCount =0
+  booksRef = React.createRef<HTMLDivElement>();
+  filteredBooksCount = 0;
 
-    state ={
-        countToDisplay:50
-    }
+  state = {
+    countToDisplay: 50,
+  };
 
-    componentDidMount() {
-        window.onscroll =() =>{
-            const books = this.booksRef.current as unknown as HTMLElement
+  componentDidMount() {
+    window.onscroll = () => {
+      const books = this.booksRef.current as unknown as HTMLElement;
 
-            if(books){
-                const toBooksBottom = books.getBoundingClientRect().bottom - window.innerHeight
-                if(toBooksBottom < 200 && this.filteredBooksCount > this.state.countToDisplay){
-                    this.setState({countToDisplay: this.state.countToDisplay + 50})
-                }
-            }
+      if (books) {
+        const toBooksBottom =
+          books.getBoundingClientRect().bottom - window.innerHeight;
+        if (
+          toBooksBottom < 200 &&
+          this.filteredBooksCount > this.state.countToDisplay
+        ) {
+          this.setState({ countToDisplay: this.state.countToDisplay + 50 });
         }
-        
-    }
+      }
+    };
+  }
 
-    componentWillUnmount() {
-        window.onscroll = null
-    }
+  componentWillUnmount() {
+    window.onscroll = null;
+  }
 
-    
+  filterBooks = (): Book[] => {
+    const { tab, books, tags, booksInProgressIds, booksDoneIds } = this.props;
+    return books.filter(
+      (book) =>
+        ((tab === "toread" && !book.moved) ||
+          (tab === "inprogress" && booksInProgressIds.has(book.id)) ||
+          (tab === "done" && booksDoneIds.has(book.id))) &&
+        (!tags.size || Array.from(tags).every((tag) => book.tags.includes(tag)))
+    );
+  };
 
-    return (<div></div>);
+  
 }
